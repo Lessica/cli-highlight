46b8c3d9978bd1726d02fc9b2db3d94e24e95c95
82Flex <82flex@gmail.com>
2022-08-25 14:39:47 +0800

    feat: add terminator option for stdin

--- src/cli.ts
+++ src/cli.ts
@@ -6,20 +6,26 @@ import { highlight, HighlightOptions, supportsLanguage } from '.'
 import { parse } from './theme'
 
 yargs
-    .option('theme', {
-        alias: 't',
-        nargs: 1,
-        description: 'Use a theme defined in a JSON file',
-    })
     .usage(
         ['', 'Usage: highlight [options] [file]', '', 'Outputs a file or STDIN input with syntax highlighting'].join(
             '\n'
         )
     )
+    .option('theme', {
+        alias: 't',
+        nargs: 1,
+        description: 'Use a theme defined in a JSON file',
+    })
     .option('language', {
         alias: 'l',
         nargs: 1,
         description: 'Set the langugage explicitely\nIf omitted will try to auto-detect',
+    })
+    .option('terminator', {
+        alias: 'e',
+        default: '\0',
+        nargs: 1,
+        description: 'Set the terminator character',
     })
     .version()
     .help('help')
@@ -29,24 +35,47 @@ yargs
 interface Arguments extends yargs.Arguments {
     theme?: string
     language?: string
+    terminator?: string
 }
 
 const argv: Arguments = yargs.argv
+const filePath = argv._[0]
+const highlightTheme = argv.theme ? fs.readFileSync(argv.theme, 'utf8') : undefined
+const options: HighlightOptions = {
+    ignoreIllegals: true,
+    theme: (highlightTheme && parse(highlightTheme)) || undefined,
+}
+if (filePath) {
+    const fileExtension = path.extname(filePath).slice(1)
+    if (fileExtension && supportsLanguage(fileExtension)) {
+        options.language = fileExtension
+    }
+}
+options.language = argv.language
 
-const file = argv._[0]
+let printCode = ((codeString: string) => {
+    process.stdout.write(highlight(codeString, options), (error: any) => (error ? console.error(error) : null))
+});
 
-let codePromise: Promise<string>
-if (!file && !(process.stdin as tty.ReadStream).isTTY) {
-    // Input from STDIN
+if (!filePath && !(process.stdin as tty.ReadStream).isTTY) {
     process.stdin.setEncoding('utf8')
+
     let code = ''
     process.stdin.on('readable', () => {
         const chunk = process.stdin.read()
         if (chunk !== null) {
             code += chunk
         }
+        if (argv.terminator) {
+            const terminator = argv.terminator
+            if (code.endsWith(terminator)) {
+                printCode(code)
+                code = ''
+            }
+        }
     })
-    codePromise = new Promise(resolve => {
+
+    new Promise<string>(resolve => {
         process.stdin.on('end', () => {
             const chunk = process.stdin.read()
             if (chunk !== null) {
@@ -55,35 +84,20 @@ if (!file && !(process.stdin as tty.ReadStream).isTTY) {
             resolve(code)
         })
     })
-} else if (file) {
-    // Read file
-    codePromise = fs.readFile(file, 'utf-8')
-} else {
-    yargs.showHelp()
-    process.exit(1)
-}
-
-Promise.all<string, string | undefined>([codePromise, argv.theme ? fs.readFile(argv.theme, 'utf8') : undefined])
-    .then(([code, theme]) => {
-        const options: HighlightOptions = {
-            ignoreIllegals: true,
-            theme: (theme && parse(theme)) || undefined,
-        }
-        if (file) {
-            const extension = path.extname(file).slice(1)
-            if (extension && supportsLanguage(extension)) {
-                options.language = extension
-            }
-        }
-        options.language = argv.language
-        return new Promise<void>((resolve, reject) =>
-            process.stdout.write(highlight(code, options), (error: any) => (error ? reject(error) : resolve()))
-        )
-    })
-    .then(() => {
+    .then(codeString => {
+        printCode(codeString)
         process.exit(0)
     })
-    .catch((error: any) => {
+    .catch(error => {
         console.error(error)
         process.exit(1)
     })
+}
+else if (filePath) {
+    printCode(fs.readFileSync(filePath, 'utf-8'))
+    process.exit(0)
+}
+else {
+    yargs.showHelp()
+    process.exit(1)
+}
